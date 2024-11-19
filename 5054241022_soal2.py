d20ae9e0f8e7735eb357dcd4b4f6352dcf755718
royan <royanjunior@gmail.com>
2024-11-20 03:49:45 +0700

    soal 2

--- 5054241022_soal2.py
+++ 5054241022_soal2.py
@@ -1,1 +1,16 @@
 #Combination
+def combine(n,k):
+    def backtrack(first = 1, curr = []):
+        if len(curr) == k:
+            output.append(curr[:])
+            return
+        for i in range(first, n + 1):
+            curr.append(i)
+            backtrack(i + 1, curr)
+            curr.pop()
+    output = []
+    backtrack()
+    return output
+
+n,k = map(int,input().split())
+print(combine(n,k))
